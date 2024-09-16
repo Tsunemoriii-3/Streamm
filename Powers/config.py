aaf4e5af4a438d925d3ec8bdf9ac16f869173b5b
Tsunemoriii-3 <157890669+Tsunemoriii-3@users.noreply.github.com>
2024-09-15 20:46:19 -0700

    Update config.py

--- Powers/config.py
+++ Powers/config.py
@@ -4,7 +4,7 @@ API_ID = int(getenv("API_ID", "20628383"))
 API_HASH = getenv("API_HASH", "65a242463b8af9ba7b3c41d8de9738d1")
 BOT_TOKEN = getenv("BOT_TOKEN", "7066626001:AAHEUmbfZpVYceXZGQJ8QYJ9JR5l01ijOsY")
 SUDO = [int(i.strip()) for i in getenv("SUDO", "962802323 1315219809 1355116689 7283633166 1432756163 1344569458 1446111611 682111519").strip().split()]
-OWNER_ID = int(getenv("1446111611"))
+OWNER_ID = int(getenv("OWNER", 1446111611))
 FSUB_CHANNEL = [int(i.strip())
                 for i in getenv("FSUB_CHANNEL", "-1002402726494").strip().split()]
 REQ_FSUB = [int(i.strip()) for i in getenv("REQ_FSUB", "-1002467950579").strip().split()]
